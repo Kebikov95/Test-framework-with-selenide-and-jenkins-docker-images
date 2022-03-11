1f3c0dc43883c8429e81b81420ecf77dc19efece
Kiryl Kebikau <kiryl.kebikau@godel.shellenergy.co.uk>
2022-03-11 17:14:30 +0300

    feat: change docker-compose.yml

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -39,7 +39,7 @@ class BaseTest {
                 "enableVideo", false
         ));
         driver = new RemoteWebDriver(
-                URI.create("http://localhost:4444/wd/hub").toURL(),
+                URI.create("http://127.0.0.1:4444/wd/hub").toURL(),
                 capabilities
         );
     }
