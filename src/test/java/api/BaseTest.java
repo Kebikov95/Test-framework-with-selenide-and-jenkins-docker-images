7bcf163177b47a1bad2cb61b840de8f27a119b01
Kiryl Kebikau <kiryl.kebikau@godel.shellenergy.co.uk>
2022-03-29 18:11:57 +0300

    feat: add api tests

---
+++ src/test/java/api/BaseTest.java
@@ -0,0 +1,9 @@
+package api;
+
+import org.apache.logging.log4j.Logger;
+
+import static org.apache.logging.log4j.LogManager.getLogger;
+
+public class BaseTest {
+    protected static final Logger log = getLogger(ui.BaseTest.class);
+}
---
+++ src/test/java/api/RestAssuredTests.java
@@ -0,0 +1,78 @@
+package api;
+
+import api.content.Payload;
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static java.lang.String.format;
+import static org.hamcrest.Matchers.equalTo;
+
+public class RestAssuredTests extends BaseTest {
+
+    String placeId = "be4991b3bc9b4d0a718ac563c49ed483";
+    String address = "Gomel";
+
+    @BeforeEach
+    public void setup() {
+        RestAssured.baseURI = "https://rahulshettyacademy.com";
+    }
+
+    @Test
+    void basePostRequestTest() {
+        String response =
+                given()
+                    .queryParam("key", "qaclick123")
+                    .header("Content-Type", "application/json")
+                    .body(Payload.getAddPlaceBody())
+                .when()
+                    .post("maps/api/place/add/json")
+                .then()
+                    .assertThat()
+                        .statusCode(200)
+                        .body("scope", equalTo("APP"))
+                        .header("server", "Apache/2.4.18 (Ubuntu)")
+                .extract().response().asString();
+
+        log.info("Response: {}", response);
+        JsonPath json = new JsonPath(response);
+        log.info("status: {}", json.getString("place_id"));
+    }
+
+    @Test
+    void basePutRequestTest() {
+        String response =
+                given()
+                        .queryParam("key", "qaclick123")
+                        .header("Content-Type", "application/json")
+                        .body(format(Payload.getAddAddressBody(), placeId, address))
+                .when()
+                        .put("maps/api/place/update/json")
+                .then()
+                        .assertThat().statusCode(200)
+                        .body("msg", equalTo("Address successfull updated"))
+                .extract().response().asString();
+
+        log.info("Response: {}", response);
+    }
+
+    @Test
+    void baseGetRequestTest() {
+        String response =
+                given()
+                        .queryParam("key", "qaclick123")
+                        .queryParam("place_id", placeId)
+                .when()
+                        .get("maps/api/place/get/json")
+                .then()
+                        .assertThat().statusCode(200)
+                        .body("address", equalTo(address))
+                .extract().response().asString();
+
+        log.info("Response: {}", response);
+        JsonPath json = new JsonPath(response);
+        log.info("Address: {}", json.getString("address"));
+    }
+}
