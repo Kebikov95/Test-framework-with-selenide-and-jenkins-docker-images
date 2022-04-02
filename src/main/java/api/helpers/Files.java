94647fdc42c990ec8ce466bb8279f8b57962236e
kirillkebikov <kebikov1995@gmail.com>
2022-04-02 17:46:32 +0300

    ref: change path to file from String to Path

--- src/main/java/api/helpers/Files.java
+++ src/main/java/api/helpers/Files.java
@@ -2,10 +2,13 @@ package api.helpers;
 
 import org.apache.logging.log4j.Logger;
 
-import java.io.*;
 import io.restassured.path.json.JsonPath;
+
+import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import static org.apache.logging.log4j.LogManager.getLogger;
 
@@ -17,22 +20,22 @@ public class Files {
     }
 
     public static JsonPath readJson(String pathToFile) throws IOException {
-        String str = readString(pathToFile);
+        String str = readString(Paths.get(pathToFile));
         return new JsonPath(str);
     }
 
-    public static String readString(String pathToFile) throws IOException {
-        log.debug("Get file from resources by path {}", pathToFile);
-        InputStream inputStream = getFileFromResourcesAsStream(pathToFile);
+    public static String readString(Path path) throws IOException {
+        log.debug("Get file from resources by path {}", path);
+        InputStream inputStream = getFileFromResourcesAsStream(path);
         return convertStreamToString(inputStream);
     }
 
-    private static InputStream getFileFromResourcesAsStream(String pathToFile) {
+    private static InputStream getFileFromResourcesAsStream(Path path) {
         ClassLoader classLoader = Files.class.getClassLoader();
-        InputStream inputStream = classLoader.getResourceAsStream(pathToFile);
+        InputStream inputStream = classLoader.getResourceAsStream(path.toString());
 
         if (inputStream == null)
-            throw new IllegalArgumentException("The filed not found by path: " + pathToFile);
+            throw new IllegalArgumentException("The filed not found by path: " + path);
 
         return inputStream;
     }
@@ -41,9 +44,9 @@ public class Files {
         StringBuilder textBuilder = new StringBuilder();
         try (Reader reader = new BufferedReader(new InputStreamReader
                 (inputStream, Charset.forName(StandardCharsets.UTF_8.name())))) {
-            int c = 0;
-            while ((c = reader.read()) != -1) {
-                textBuilder.append((char) c);
+            int count;
+            while ((count = reader.read()) != -1) {
+                textBuilder.append((char) count);
             }
         }
         return textBuilder.toString();
