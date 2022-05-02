8dfe94a03bfe776cee509cfc64bda4725b9755c7
Kiryl Kebikau <kiryl.kebikau@godel.shellenergy.co.uk>
2022-05-02 16:06:31 +0300

    ref: change logs for DB methods

--- src/test/java/db/DataBaseTests.java
+++ src/test/java/db/DataBaseTests.java
@@ -19,8 +19,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class DataBaseTests {
 
     private static final Logger log = getLogger(DataBaseTests.class);
-
-    private UserDaoImplementation userDaoImpl = new UserDaoImplementation();
+    private static final UserDaoImplementation USER_DAO_IMPL = new UserDaoImplementation();
 
     @BeforeAll
     static void createDb() {
@@ -46,74 +45,77 @@ class DataBaseTests {
                 .password("23212321")
                 .email("merc23@yahoo.com")
                 .build();
-        boolean response = userDaoImpl.create(user);
-        log.info("The user has been added, result: {}", response);
+        boolean response = USER_DAO_IMPL.create(user);
         assertTrue(response, "The user hasn't been added.");
+        User dbUser = USER_DAO_IMPL.findUserByUserName(user.getUserName());
+        assertEquals(user.getUserName(), dbUser.getUserName());
+        log.info("The user has been added: [{}]", user);
     }
 
     @Test
     void addUsers() throws DaoException {
-        List<User> addingUsers = List.of(
+        List<User> users = List.of(
                 new User(10, "Alex97", "aLex", "alex@mail.com"),
                 new User(11, "GK_2002", "qwerty", "gk@mail.com")
         );
-        userDaoImpl.create(addingUsers);
-        List<User> readingUsers = userDaoImpl.findAll();
-        boolean isContains = readingUsers.containsAll(addingUsers);
+        USER_DAO_IMPL.create(users);
+        List<User> dbUsers = USER_DAO_IMPL.findAll();
+        boolean isContains = dbUsers.containsAll(users);
         assertTrue(isContains);
+        log.info("The users has been added: [{}]", users);
     }
 
     //READ
     @Test
     void getSizeOfUsers() throws DaoException {
-        List<User> users = userDaoImpl.findAll();
-        log.info("The quantity of users is '{}'", users.size());
+        List<User> users = USER_DAO_IMPL.findAll();
         int expectedQuantity = 5;
         int actualQuantity = users.size();
         assertEquals(expectedQuantity, actualQuantity);
+        log.info("The quantity of users is '{}'", users.size());
     }
 
     @Test
     void getUserById() throws DaoException {
         long firstUserId = 1;
-        User user = userDaoImpl.findEntityById(firstUserId);
-        log.info("The first user: {}", user);
+        User user = USER_DAO_IMPL.findEntityById(firstUserId);
         assertNotNull(user);
+        log.info("The user by id '{}': [{}]", firstUserId, user);
     }
 
     @Test
     void getUserByUserName() throws DaoException {
         String userName = "FrankUK";
-        User user = userDaoImpl.findUserByUserName(userName);
-        log.info("The first user: {}", user);
+        User user = USER_DAO_IMPL.findUserByUserName(userName);
         assertNotNull(user);
+        log.info("Get user by name '{}': [{}]", userName, user);
     }
 
     // UPDATE
     @Test
     void updateFirstUser() throws DaoException {
         long firstUserId = 2;
-        User userBeforeUpdate = userDaoImpl.findEntityById(firstUserId);
+        User userBeforeUpdate = USER_DAO_IMPL.findEntityById(firstUserId);
+        log.info("The user before updating: [{}]", userBeforeUpdate);
         userBeforeUpdate.setUserName("Android_101");
-        userDaoImpl.update(userBeforeUpdate);
-        User userAfterUpdate = userDaoImpl.findEntityById(firstUserId);
+        USER_DAO_IMPL.update(userBeforeUpdate);
+        User userAfterUpdate = USER_DAO_IMPL.findEntityById(firstUserId);
         assertEquals(userBeforeUpdate, userAfterUpdate);
+        log.info("The user after updating: [{}]", userAfterUpdate);
     }
 
     // DELETE
     @Test
     void deleteUserById() throws DaoException {
         long firstUserId = 1;
-        User user = userDaoImpl.findEntityById(firstUserId);
-        log.info("The first user: {}", user);
+        User user = USER_DAO_IMPL.findEntityById(firstUserId);
         assertNotNull(user);
 
-        boolean response = userDaoImpl.delete(user.getId());
-        log.info("The user has been deleted, result: {}", response);
+        boolean response = USER_DAO_IMPL.delete(user.getId());
         assertTrue(response, "The user hasn't been deleted.");
 
-        user = userDaoImpl.findEntityById(firstUserId);
-        log.info("The first user: {}", user);
+        user = USER_DAO_IMPL.findEntityById(firstUserId);
         assertNull(user);
+        log.info("The user has been deleted by id '{}', user = [{}]", firstUserId, user);
     }
 }
